b993883463dd2ec7d341b27ba9219afae61fd65c
Ali Zadil <tarikz94@gmail.com>
2023-08-25 21:11:14 +0700

    app updated

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// styles
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
+// Layouts
+import { Navbar } from "./Components/Navbar";
+import { Footer } from "./Components/Footer";
+
+// Pages
+import { Homepage } from "./Pages/Homepage";
+import { BusinessImprovement } from "./Pages/BusinessImprovement";
+import { TeamForExecution } from "./Pages/TeamForExecution";
+import { PeopleAgenda } from "./Pages/PeopleAgenda";
+import { BoardAdvisory } from "./Pages/BoardAdvisory";
+import { Contact } from "./Pages/Contact";
+import { MeteZadil } from "./Pages/MeteZadil";
+import { Info } from "./Pages/Info";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="*" element={<Homepage />} />
+        <Route path="/board-advisory" element={<BoardAdvisory />} />
+        <Route path="/people-agenda" element={<PeopleAgenda />} />
+        <Route path="/tea-for-execution" element={<TeamForExecution />} />
+        <Route path="/BusinessImprovement" element={<BusinessImprovement />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/mete-zadil" element={<MeteZadil />} />
+        <Route path="/info" element={<Info />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
